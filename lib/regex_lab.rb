6eacfdb15967c9e088f71899a430b305275333da
yaoyu bao <yaobao5354@gmail.com>
2019-08-18 20:48:05 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,12 +1,18 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+  if word.scan(/\A[aeiouAEIOU]/) != []
+    return true
+  end
+  false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+   text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
+  binding.pry
+  text.scan((/^\w{5}/))
 
 end
 
